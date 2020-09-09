8e104f7248125cb5020472b47551df5d50947211
rootDEV2990 <website.dev@icloud.com>
2020-09-09 12:55:23 -0500

    New DB migration and show method and view for new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,2 @@
 class Article < ApplicationRecord
-  def change
-    create_table :articles do |t|
-      t.string :title
-      t.text :body
-  
-      t.timestamps
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_160750) do
+ActiveRecord::Schema.define(version: 2020_09_09_174337) do
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
