fda8599687351060845c3fd0298bc582c35a12d6
rootDEV2990 <website.dev@icloud.com>
2020-09-14 17:48:11 -0500

    Added pictures and conditionals to display pics in view

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
     has_many :comments
     has_many :taggings
     has_many :tags, through: :taggings
+    has_attached_file :image
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
     def tag_list
       self.tags.collect do |tag|
         tag.name
